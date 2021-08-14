adef281d6f2919bdbede04c1d50c9685d4b05cae
lamine <lemzogueye.lg72@gmail.com>
2021-08-14 05:57:56 +0000

    Intégration de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gestion-livres';
+  constructor(){
+    const config = {
+      apiKey: "AIzaSyC1wTGnbPafL4Fx2xNdMs6iYQ0CBQ4gZUo",
+      authDomain: "gestion-livres-14180.firebaseapp.com",
+      projectId: "gestion-livres-14180",
+      storageBucket: "gestion-livres-14180.appspot.com",
+      messagingSenderId: "875428021967",
+      appId: "1:875428021967:web:02bdf65a9e66f78c0799ba",
+      measurementId: "G-Y4GKLBKMLC"
+    };
+    firebase.default.initializeApp(config);
+  }
 }
